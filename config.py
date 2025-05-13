1bded4cf1a91d61ec122d4f53ecca19eef787321
Bakir Lakaf <89250704+bakirLakaf@users.noreply.github.com>
2025-05-13 20:58:56 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+ADMIN_BOT_TOKEN = "8109108612:AAFFuJJ1M3NKx9iersr5Hk3UHM8vrKJQ9F8"  # توكن بوت الأدمن
+CUSTOMER_BOT_TOKEN = "7967668983:AAH-nGDTESzRbFprIHWjj-AOUxfyyXWuDv8"  # توكن بوت الزبائن
+MAIN_ADMIN_ID = 1144959915  # معرف الأدمن الرئيسي
+SECONDARY_ADMIN_ID = 1037341709  # معرف الأدمن الفرعي
